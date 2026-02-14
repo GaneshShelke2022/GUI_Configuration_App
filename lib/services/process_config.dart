f6171787d3059208f209a1a4db71a6409702eac4
GaneshShelke2022 <shelkeganesh2022@gmail.com>
2026-02-14 12:46:20 +0530

    ninth commit

--- lib/services/process_config.dart
+++ lib/services/process_config.dart
@@ -56,6 +56,7 @@ class ProcessConfig {
  
   // this is updated configuration page  /////////////////////////////////////////////////////////////////////////////////////////ss
   // updated 
+  // New updates
     // Determine Address based on broadcast
     // Python: if broadcast: 0x0000, else 0x0000
     final int address = isBroadcast ? 0x0000 : 0x0000; // Both cases use 0x0000 in the original code and both are with 00 address for hex file modification
